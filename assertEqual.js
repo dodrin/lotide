f4ff2f3b73ec2005fb7a50ac7dbfb35033638b4d
Rina Inada <indrn@icloud.com>
2023-05-21 05:52:15 +0000

    refactored assertEqual for better practice

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  if (actual !== expected) {
     console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   }
 };
 
