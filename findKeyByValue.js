91f6061b5ba0238498ffbb7c103e2d3203df69a9
Rina Inada <indrn@icloud.com>
2023-05-27 01:08:14 +0000

    first version of findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+//COPY assertEqual FUNCTION
+const assertEqual = (actual, expected) => {
+  if (actual !== expected) {
+    console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+//FUNTION IMPLEMENTATION
+const findKeyByValue = (obj, value) => {
+  for (const key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+//TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
