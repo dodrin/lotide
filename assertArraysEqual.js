bf6c8ead12decc3c17b4be89fb3c8960eda72f29
Rina Inada <indrn@icloud.com>
2023-06-07 03:19:56 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,25 @@
-//FUNCTION IMPLEMENTAION
 const assertArraysEqual = function(arr1, arr2) {
   let failMsg = `😭😭Assertion Failed: ${arr1} !== ${arr2}`;
   let passMsg = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`;
+
+  if (arr1 === undefined || arr2 === undefined) {
+    console.log(failMsg);
+    return false;
+  }
 
   if (arr1.length !== arr2.length) {
     console.log(failMsg);
     return false;
   }
-  
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       console.log(failMsg);
       return false;
     }
   }
-  console.log(passMsg)
+  console.log(passMsg);
   return true;
 };
 
-//test
-asserArraysEqual([1, 2, 3], [1, 2, 3]);
-asserArraysEqual([1, 2, 3], [3, 2, 1]);
-
-asserArraysEqual(["1", "2", "3"], ["1", "2", "3"]); 
-asserArraysEqual(["1", "2", "3"], ["1", "2", 3]); 
+module.exports = assertArraysEqual;
