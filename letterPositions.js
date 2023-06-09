596e25ed8e947c8def7ab23678d138370676a891
Rina Inada <indrn@icloud.com>
2023-06-09 05:39:10 +0000

    removed assertArraysEqual and eqArrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,36 +1,3 @@
-//COPY eqArrays and asserArraysEqual
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; arr1.length > i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (arr1, arr2) => {
-  let failMsg = `😭😭Assertion Failed: ${arr1} !== ${arr2}`;
-  let passMsg = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`;
-
-  if (arr1.length !== arr2.length) {
-    console.log(failMsg);
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(failMsg);
-      return false;
-    }
-  }
-  console.log(passMsg);
-  return true;
-};
-
 //FUNCTION IMPLEMENTATION
 const letterPositions = (sentence) => {
   const result = {};
