6dc5da9fc0756063b41a56f6eda6b923f659427a
Rina Inada <indrn@icloud.com>
2023-06-10 01:24:55 +0000

    cleaned with require and removed unnecessary comments

--- findKey.js
+++ findKey.js
@@ -1,11 +1,4 @@
-//COPY OF assertEqual
-const asserEqual = (actual, expected) => {
-  if (actual !== expected) {
-    console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+const asserEqual = require("./assertEqual");
 
 //FUNCTION IMPLEMENTATION
 //1. function which takes an object and a callback
@@ -30,16 +23,6 @@ let testobj = {
   Ora: { stars: 2 },
   Akelarre: { stars: 3 },
 };
-/*
-findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
-*/
 
 asserEqual(
   findKey(testobj, (x) => x.stars === 2),
