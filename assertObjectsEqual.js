5573c820667abdfd241a1d60ae3c2d4677063bba
Rina Inada <indrn@icloud.com>
2023-06-10 01:21:41 +0000

    cleaned up with require

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,40 +1,5 @@
-//COPY eqArrays 
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; arr1.length > i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-//COPY eqobjects
-const eqObjects = function(object1, object2) {
-  const key1 = Object.keys(object1);
-  const key2 = Object.keys(object2);
-
-  if (key1.length !== key2.length) {
-    return false;
-  }
-
-  for (const key of key1) {
-    let value1 = object1[key];
-    let value2 = object2[key];
-    
-    if (Array.isArray(value1) && Array.isArray(value2)) {
-      return eqArrays(value1, value2);
-    }
-
-    if (value1 !== value2) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
 
 //FUCNTION IMPLEMENTATION
 const assertObjectsEqual = (actual, expected) => {
