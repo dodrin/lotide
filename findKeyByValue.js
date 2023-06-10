630fe142120d2c7b70d8600af36e99cd4adff9bd
Rina Inada <indrn@icloud.com>
2023-06-10 00:34:35 +0000

    implemented M&C test

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
-//COPY assertEqual FUNCTION
-const assertEqual = (actual, expected) => {
-  if (actual !== expected) {
-    console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
-
-//FUNTION IMPLEMENTATION
 const findKeyByValue = (obj, value) => {
   for (const key in obj) {
     if (obj[key] === value) {
@@ -16,12 +6,4 @@ const findKeyByValue = (obj, value) => {
   }
 };
 
-//TEST CODE
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
