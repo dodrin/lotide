314cff59e8ad96e1e9a0cd5f493ebea325c8b60c
Rina Inada <indrn@icloud.com>
2023-06-10 01:29:57 +0000

    cleaned with require

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+const assertArraysEqual = require("./assertArraysEqual");
 //FUCTION DEFENITION
 // Our map function will take in two arguments:
 // 1. An array to map
@@ -14,26 +15,6 @@ const map = function(array, callback) {
  return results;
 };
 const result = map(words, word => word[0]);
-console.log(result); 
 
-//COPY assertArraysEqual
-const assertArraysEqual = function(arr1, arr2) {
-  let failMsg = `😭😭Assertion Failed: ${arr1} !== ${arr2}`;
-  let passMsg = `✅✅✅Assertion Passed: ${arr1} === ${arr2}`;
-
-  if (arr1.length !== arr2.length) {
-    console.log(failMsg);
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(failMsg);
-      return false;
-    }
-  }
-  console.log(passMsg)
-  return true;
-};
 const expected = ['m', 'b', 't', 'r', 'd', 's', 'h', 's', 'm', 'h', 'd', 'p'];
 assertArraysEqual(result, expected);
