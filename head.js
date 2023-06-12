6f6be43faa8602bb14814726226a919e83971a38
Rina Inada <indrn@icloud.com>
2023-06-12 19:46:44 +0000

    Update comment

--- head.js
+++ head.js
@@ -1,3 +1,7 @@
+//The function takes an array as parameter
+//return the "head", index 0 element
+//If the array is empty returns undefined
+
 const head = function (array) {
   if (array === []) {
     return undefined;
