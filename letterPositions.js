d0ccd1be65dfa1d4e218d8835fb4b6060ab5058f
Rina Inada <indrn@icloud.com>
2023-06-12 19:51:42 +0000

    Add comments

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,6 @@
+//The function takes an sentence/string as parameter
+//It will iterate through the sentence and store each letter positions into a new object
+//result will be the new object with letters as keys, letter positions as values
 const letterPositions = (sentence) => {
   const result = {};
   for (let i = 0; i < sentence.length; i++) {
