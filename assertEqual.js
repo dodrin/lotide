f3ca932482d218b128ad6282c1a078f8dfcaee23
Rina Inada <indrn@icloud.com>
2023-06-12 19:57:40 +0000

    add comment

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+//The function takes 2 values as parameters. returns if they match or not
+
 const assertEqual = (actual, expected) => {
   if (actual !== expected) {
     console.log(`😭Assertion Failed: ${actual} !== ${expected}`);
