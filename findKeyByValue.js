a28f94accb4ca06ee96a8dd83cb442d3975680ed
Rina Inada <indrn@icloud.com>
2023-06-12 19:42:39 +0000

    Update Comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,5 @@
+//The function takes an object and a value as parameters
+//Returns the key that containes the given value
 const findKeyByValue = (obj, value) => {
   for (const key in obj) {
     if (obj[key] === value) {
