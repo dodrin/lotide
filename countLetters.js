34ea2844d72494afb3b51c0c9114bcec3e61a97e
Rina Inada <indrn@icloud.com>
2023-06-12 19:42:00 +0000

    Update comments

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,11 @@
+//The function takes a string/sentence as parameter
+//Iterate over the string and count the charactere(s)
+//It will not count " ". It will return the number of character(s)
+
 const countLetters = (str) => {
   const result = {};
   for (const char of str) {
-    //skip the operation if the character is " "
     if (char !== " ") {
-      //check if the character already exist in the result object
-      //if not, set a value to 1, if already exist count up
       if (!result[char]) {
         result[char] = 1;
       } else {
