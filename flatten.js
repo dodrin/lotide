0c2fa79913c8fa6dd671dbf55b803047cfbe9cc1
Rina Inada <indrn@icloud.com>
2023-06-12 19:32:14 +0000

    add comments

--- flatten.js
+++ flatten.js
@@ -1,3 +1,7 @@
+//takes in an array(inputArr) as a parameter, the array's elements includes neseted array
+//Returns a flattend/unnested version of the inputArr
+//It can only handle one level of nesting
+
 const flatten = (inputArr) => {
   const result = [];
   for (let i = 0; i < inputArr.length; i++) {
